4082aff052e633b291ba32719bb227ae1110964a
Profster69 <Prof69[200~@riseup.net>
2024-02-07 15:38:35 +0100

    Browser-Wallet-Start

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Mint from '../views/Mint.vue'
 import Create from '../views/Create.vue'
 import Tokens from '../views/Tokens.vue'
 import Users from '../views/Users.vue'
+import Inscriptions from '../views/Inscriptions.vue'
 
 const routes = [
   { path: '/', component: Home, meta: { title: 'Mint' } },
@@ -14,6 +15,7 @@ const routes = [
   { path: '/users/:wallet', name: 'user', component: Users, meta: { title: 'User Profile' }, props: true },
   { path: '/trade', component: ComingSoon, meta: { title: 'Marketplace' } },
   { path: '/mint/:protocol/:tick', name: 'mint', component: Mint, meta: { title: 'Mint Token' }, props: true },
+  { path: '/inscriptions/:protocol/:tick', name: 'inscriptions', component: Inscriptions, meta: { title: 'Inscription Details' }, props: true },
   { path: '/create', component: Create, meta: { title: 'Create Inscription'}},
   { path: '/tokens', component: Tokens, meta: { title: 'Inscriptions'}}
 ]
---
+++ src/views/Inscriptions.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="mx-auto w-full max-w-6xl py-12 text-center px-4">
+        <span class="lowercase tracking-wider inline-block whitespace-nowrap rounded-[0.27rem] px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.60em] font-bold leading-none text-neutral-50" :class="props.protocol === 'tzrc-20' ? 'bg-slate-700' :  'bg-lightblue'">{{ props.protocol }}</span> 
+
+        <h1 class="text-4xl lg:text-5xl font-light mb-8 tracking-wider">{{ props.tick.toUpperCase() }}</h1>
+
+        <button @click="collect" class="btn-primary">Collect</button>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+// import { toast } from 'vue3-toastify'
+// import BigNumber from 'bignumber.js'
+// import { api } from '../util/api'
+
+const props = defineProps({protocol:String, tick:String})
+
+console.log(props)
+
+const load = async () => {
+    try {
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+onMounted(async () => {
+    load()
+})
+
+onBeforeUnmount(() => {
+})
+</script>
+
+<style scoped>
+</style>
